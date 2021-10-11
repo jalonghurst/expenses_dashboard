c7a7fa7e3219cf35865317b40593e0ab351264ab
jalonghurst <jacqui.longhurst@btinternet.com>
2021-10-11 11:46:04 +0200

    check

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+
 const App = () => {
   const expenses = [
     {
